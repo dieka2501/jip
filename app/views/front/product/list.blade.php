c83a19be6a9875432df00146aa24c605a55468e3
dikdik <dieka.koes@gmail.com>
2016-01-09 20:31:36 +0700

    produk

--- app/views/front/product/list.blade.php
+++ app/views/front/product/list.blade.php
@@ -6,8 +6,7 @@
 Our Products
 <ol class="breadcrumb">
   <li><a href="#">Home</a></li>
-  <li><a href="#">Sample</a></li>
-  <li class="active">Breadcrumb</li>
+  <li class="active"><a href="#">Product</a></li>
 </ol>
 </div>
 </div>
@@ -17,7 +16,33 @@ Our Products
 		<div class="row">
 		<div class="col-md-12">
 		<div class="menu-produk">
+		@for($i = 0;$i < $cacah_cat; $i++)
+			<?php 
+				$j = ($i*5);
+				$k = ($i*5)+1;
+				$l = ($i*5)+2;
+				$m = ($i*5)+3;
+				$n = ($i*5)+4;
+				$cat1 = (isset($cat_all[$j]->category_name))? $cat_all[$j]->category_name : "";
+				$cat2 = (isset($cat_all[$k]->category_name))? $cat_all[$k]->category_name : "";
+				$cat3 = (isset($cat_all[$l]->category_name))? $cat_all[$l]->category_name : "";
+				$cat4 = (isset($cat_all[$m]->category_name))? $cat_all[$m]->category_name : "";
+				$cat5 = (isset($cat_all[$n]->category_name))? $cat_all[$n]->category_name : "";
+				// var_dump($cat1);die;
+			?>
 			<div class="col-md-4">
+			<div class="list-group">
+			  <a href="{{Config::get('app.url')}}public/product" class="list-group-item">
+			    {{$cat1}}
+			  </a>
+			  <a href="{{Config::get('app.url')}}public/product" class="list-group-item">{{$cat2}}</a>
+			  <a href="{{Config::get('app.url')}}public/product" class="list-group-item">{{$cat3}}</a>
+			  <a href="{{Config::get('app.url')}}public/product" class="list-group-item">{{$cat4}}</a>
+			  <a href="{{Config::get('app.url')}}public/product" class="list-group-item">{{$cat5}}</a>
+			</div>
+			</div>
+		@endfor
+			<!-- <div class="col-md-4">
 			<div class="list-group">
 			  <a href="#" class="list-group-item">
 			    Cras justo odio
@@ -27,8 +52,8 @@ Our Products
 			  <a href="#" class="list-group-item">Porta ac consectetur ac</a>
 			  <a href="#" class="list-group-item">Vestibulum at eros</a>
 			</div>
-			</div>
-			<div class="col-md-4">
+			</div> -->
+			<!-- <div class="col-md-4">
 			<div class="list-group">
 			  <a href="#" class="list-group-item">
 			    Cras justo odio
@@ -38,18 +63,7 @@ Our Products
 			  <a href="#" class="list-group-item">Porta ac consectetur ac</a>
 			  <a href="#" class="list-group-item">Vestibulum at eros</a>
 			</div>
-			</div>
-			<div class="col-md-4">
-			<div class="list-group">
-			  <a href="#" class="list-group-item">
-			    Cras justo odio
-			  </a>
-			  <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
-			  <a href="#" class="list-group-item">Morbi leo risus</a>
-			  <a href="#" class="list-group-item">Porta ac consectetur ac</a>
-			  <a href="#" class="list-group-item">Vestibulum at eros</a>
-			</div>
-			</div>
+			</div> -->
 		<div style="clear:both;"></div>
 		</div>
 		</div>
@@ -72,7 +86,7 @@ Our Products
 					</p>
 					<a href="<?php echo Config::get('app.url');?>public/product/detail/{{$products->id_product}}" class="btn-readmore"><i class="fa fa-angle-right">&nbsp;</i> read more</a>
 					<div class="pull-right">
-						<a href="<?php echo Config::get('app.url');?>public/cart/add/" class="btn btn-warning btn-xs cart-btn">Add To Cart</a>
+						<a href="<?php echo Config::get('app.url');?>public/cart/do?idproduct={{$products->id_product}}" class="btn btn-warning btn-xs cart-btn">Add To Cart</a>
 					</div>
 				</div>
 			</div>
