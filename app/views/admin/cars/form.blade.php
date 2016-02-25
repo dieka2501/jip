d66b851ee4ea4fdfb59a377ee47d439617e41a7e
Dikdik <dieka.koes@gmail.com>
2016-02-25 10:50:17 +0700

    Cars view

---
+++ app/views/admin/cars/form.blade.php
@@ -0,0 +1,164 @@
+@extends('admin.template')
+@section('content')
+
+<!-- Content Header (Page header) -->
+<p>{{$notip}}</p>
+<section class="content-header">
+  <h1>
+    {{$page_name}}
+    <small>Product</small>
+  </h1>
+  <ol class="breadcrumb">
+    <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+    <li><a href="#">Forms</a></li>
+    <li class="active">{{$page_name}}</li>
+  </ol>
+</section>
+
+<!-- Main content -->
+<section class="content">
+
+  <!-- SELECT2 EXAMPLE -->
+  <div class="box box-default">
+  {{Form::open(array('url'=>$url,'files'=>true))}}
+    <div class="box-header with-border">
+      <h3 class="box-title">{{$page_name}}</h3>
+      <div class="box-tools pull-right">
+        <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+        <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-remove"></i></button>
+      </div>
+    </div><!-- /.box-header -->
+    
+    <div class="box-body">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Nama Mobil</label>
+              {{Form::text('cars_name',$cars_name,array('class'=>'form-control','placeholder'=>'Nama Mobil','required'=>'required'))}}
+              {{Form::hidden('ids',$ids,array('class'=>'form-control','placeholder'=>'Input News Title Here'))}}
+          </div>
+          <div class="form-group">
+            <label>Harga Mobil</label>
+            {{Form::text('cars_price',$cars_price,array('class'=>'form-control','placeholder'=>'Harga Mobil','required'=>'required'))}}
+          </div>
+          <div class="form-group">
+            <label>Model Mobil</label>
+            {{Form::text('cars_model',$cars_model,array('class'=>'form-control','placeholder'=>'Model Mobil'))}}
+          </div>
+          <div class="form-group">
+            <label>Brand Mobil</label>
+            {{Form::text('cars_brand',$cars_brand,array('class'=>'form-control','placeholder'=>'Brand Mobil'))}}
+          </div>
+          
+        </div><!-- /.col -->
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Kategori Mobil</label>
+            {{Form::select('cars_category',['Mobil Baru','Mobil Bekas'],$cars_category,array('class'=>'form-control','required'=>'required'))}}
+          </div>
+          <div class="form-group">
+            <label>Warna Mobil</label>
+            {{Form::text('cars_color',$cars_color,array('class'=>'form-control','placeholder'=>'Warna Mobil'))}}
+          </div>
+          <div class="form-group">
+            <label>Status Mobil</label>
+            {{Form::select('cars_status',array(1=>'Aktif',0=>'Tidak Aktif'),$cars_status,array('class'=>'form-control'))}}
+          </div>
+          <!-- /.form-group -->
+          <!-- <div class="form-group">
+            <label>Main Image</label>
+            <input type="file" class="form-control" name='main_image'>
+          </div> -->
+          <!-- <div class="form-group">
+            <label>Image 2</label>
+            <input type="file" class="form-control" name='image2'>
+          </div> -->
+          <!-- <div class="form-group">
+            <label>Image 3</label>
+            <input type="file" class="form-control" name='image3'>
+          </div> -->
+          <!-- <div class="form-group">
+            <label>Image 4</label>
+            <input type="file" class="form-control" name='image4'>
+          </div> -->
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+      <div class="row"> 
+          <div class="col-md-12">
+              <div class="form-group" id='divfile'>
+                  <label>Image</label>
+                  <input type="file" class="form-control" name='image[]' >
+                </div><!-- /.form-group -->
+                <div class="form-group">
+                  <button class="btn btn-primary" id='clone_file' type="button">Add File</button>
+              </div>
+          </div>
+      </div>
+      <div class="row">
+      <div class="col-md-12">
+      <div class="form-group">
+        <label>Spesifikasi</label>
+        <textarea class="form-control" id="cars_specification " name="cars_specification"> {{$cars_specification}}</textarea>
+      </div>
+      </div>
+      </div>
+    </div><!-- /.box-body -->
+    @if($action == 'edit')
+    <div class="box-body">
+        <table class='table'>
+            <thead>
+                <tr>
+                    <th>Image</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+             @foreach($pf as $pfs)
+              <tr>
+                <td><img src="{{Config::get('app.url')}}aset/upload/{{$pfs->image_url}}" width="50px" height="50px"></td>
+                <td class="text-left" align="left"><a href="{{Config::get('app.url')}}public/admin/product/file/delete/{{$pfs->id_pi}}?id={{$ids}}" class="btn btn-danger text-left" onclick="return confirm('Apakah Anda Yakin?')">Delete</a></td>
+              </tr>
+            @endforeach 
+            </tbody>
+        </table>
+    </div>
+    @endif
+    <div class="box-footer">
+      <a href="{{Config::get('app.url')}}public/admin/cars" class="btn btn-warning">Back</a>
+      <button style="submit" class="btn btn-success">Submit</button>
+    </div>
+    {{Form::close()}}
+  </div><!-- /.box -->
+
+</section><!-- /.content -->
+<script src="<?php echo Config::get('app.url');?>aset/js/jquery.js" type="text/javascript"></script>
+<script type="text/javascript" src="{{Config::get('app.url')}}aset/tinymce/jquery.tinymce.min.js"></script>
+<script type="text/javascript" src="{{Config::get('app.url')}}aset/tinymce/tinymce.min.js"></script>
+<script type="text/javascript" src="{{Config::get('app.url')}}aset/tinymce/plugin.min.js"></script> 
+<script type="text/javascript">
+  tinymce.init({
+    selector: "textarea",
+    menubar:false,
+    plugins: "insertdatetime",
+    plugins: "link fullscreen",
+    toolbar: [
+        "undo | redo | styleselect | bold | italic | link | fullscreen | alignleft | aligncenter |  alignright | insertdatetime | link | removeformat | cut | copy | paste | bullist | numlist | outdent | indent |"
+    ],
+    insertdatetime_formats: ["%Y.%m.%d", "%H:%M"],
+    
+ });
+</script> 
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#clone_file').click(function(){
+            var form_file = "<div class='form-group'>"+
+                            "<label>Image</label>"+
+                            "<input type='file' class='form-control' name='image[]' >"+
+                            "</div>";
+            // console.log(form_file);
+            // alert(form_file);
+            $('#divfile').append(form_file);
+        });
+    });
+</script>
+@stop
---
+++ app/views/admin/cars/table.blade.php
@@ -0,0 +1,87 @@
+@extends('admin.template')
+@section('content')
+
+<!-- Content Header (Page header) -->
+<section class="content-header">
+  <h1>
+    List Data
+    <small>Cars</small>
+  </h1>
+  <ol class="breadcrumb" style="padding-top:0px;margin-top:-5px">
+    <li><a class="btn btn-warning" href="{{Config::get('app.url')}}public/admin/cars/create"><i class="fa fa-plus"></i> Add Data</a></li>
+  </ol>
+</section>
+{{$notip}}
+<!-- Main content -->
+<section class="content">
+  <!-- Main row -->
+  <div class="row">
+    <div class="col-xs-12">
+      <div class="box">
+        <div class="box-header">
+          <h3 class="box-title">List Data</h3>
+          <div class="box-tools">
+            <div class="input-group" style="width: 150px;">
+              <input type="text" name="table_search" class="form-control input-sm pull-right" placeholder="Search">
+              <div class="input-group-btn">
+                <button class="btn btn-sm btn-default"><i class="fa fa-search"></i></button>
+              </div>
+            </div>
+          </div>
+        </div><!-- /.box-header -->
+        <div class="box-body table-responsive no-padding">
+          <table class="table table-hover">
+            <tbody><tr>
+              <th class="text-center">No</th>
+              <th class="text-center">Nama Mobil</th>
+              <th class="text-center">Price</th>  
+              <th class="text-center">Status</th>
+              <th class="text-center">Color</th>
+              <th class="text-center">Action</th>
+            </tr>
+            <?php $i = 0;?>
+              @foreach($list as $datas)
+              <?php 
+                
+                if(Input::has('page')){
+                  $page = Input::get('page') - 1;
+                }else{
+                  $page = 0;
+                }
+                $jj   = 20*$page + $i;
+                $i++;
+                $stat = ($datas->cars_status == 0)? 'Mobil Baru':'Mobil Bekas';
+              ?>
+            <tr>
+              <td width="5%">{{$i}}</td>
+              <td>{{$datas->cars_name}}</td>
+              <td>{{$datas->cars_price}}</td>
+              <td width="30%">{{$stat}}</td>
+              <td class="status">{{$datas->cars_color}}</td>
+              <td>
+              <a class="btn btn-sm btn-success" href="<?php echo Config::get('app.url');?>public/admin/cars/edit/{{$datas->id_cars}}">Edit</a> 
+              <a class="btn btn-sm btn-danger" onclick="return confirm('Are You Sure?')" href="<?php echo Config::get('app.url');?>public/admin/cars/delete/{{$datas->id_cars}}">Delete</a>
+              </td>
+            </tr>
+            @endforeach 
+            <!-- <tr>
+              <td>219</td>
+              <td>Alexander Pierce</td>
+              <td>11-7-2014</td>
+              <td><span class="label label-warning">Pending</span></td>
+              <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+              <td>
+              <a class="btn btn-sm btn-success" href="#">Edit</a> 
+              <a class="btn btn-sm btn-danger" href="">Delete</a>
+              </td>
+            </tr> -->
+          </tbody></table>
+          {{$list->links()}}
+        </div><!-- /.box-body -->
+      </div><!-- /.box -->
+    </div>
+  </div>
+
+</section><!-- /.content -->
+
+@stop
